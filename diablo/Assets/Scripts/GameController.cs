153ca72f70e668be1bc5167930e3e259b509ebb5
Jorgearbolex <jorgearbolex@gmail.com>
2019-11-13 16:45:26 -0500

    ahi va
    ahi va

---
+++ diablo/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject asteroide;
+    public Vector3 asteroidAparece;
+
+    public int cuentaAsteroides;
+
+    void Start()
+    {
+        InstaciarAsteroide ();
+    }
+
+    void InstaciarAsteroide ()
+    { 
+        for (int i = 0; i < cuentaAsteroides; i++)
+        {
+            Vector3 zonaApareceAsteroid = new Vector3 (Random.Range (-asteroidAparece.x, asteroidAparece.x), asteroidAparece.y, asteroidAparece.z);
+            Instantiate(asteroide, zonaApareceAsteroid, Quaternion.identity);
+        }
+            
+    }
+}
+
