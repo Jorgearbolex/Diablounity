ab2ce5b81f401233f4327f000d9a7f545c0d4cae
Jorgearbolex <jorgearbolex@gmail.com>
2019-11-14 16:01:52 -0500

    eso vi
    eso vi

--- diablo/Assets/Scripts/GameController.cs
+++ diablo/Assets/Scripts/GameController.cs
@@ -8,19 +8,32 @@ public class GameController : MonoBehaviour
     public Vector3 asteroidAparece;
 
     public int cuentaAsteroides;
+    float tiempoEsperaAsteroid = 2f;
+    float tiempoEsperaInicial = 1f;
+    float ordaDeAsteroides = 1f;
 
     void Start()
     {
         InstaciarAsteroide ();
+        StartCoroutine(InstaciarAsteroide());
+        
     }
 
-    void InstaciarAsteroide ()
+    IEnumerator InstaciarAsteroide ()
     { 
-        for (int i = 0; i < cuentaAsteroides; i++)
+        yield return new WaitForSeconds (tiempoEsperaInicial);
+        while (true) 
         {
+            for (int i = 0; i < cuentaAsteroides; i++)
+          {
             Vector3 zonaApareceAsteroid = new Vector3 (Random.Range (-asteroidAparece.x, asteroidAparece.x), asteroidAparece.y, asteroidAparece.z);
             Instantiate(asteroide, zonaApareceAsteroid, Quaternion.identity);
+
+            yield return new WaitForSeconds (tiempoEsperaAsteroid);
+          }
+          yield return new WaitForSeconds (ordaDeAsteroides);
         }
+        
             
     }
 }
